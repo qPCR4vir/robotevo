9329eab78a665bd62748d88f828e5b71a7a1a9f1
qPCR4vir <arielvina@yahoo.es>
2014-08-30 23:02:07 +0200

    First ideas about Robot classes (arm, tips, WT,...)

--- EvoScriPy/Robot.py
+++ EvoScriPy/Robot.py
@@ -1,2 +1,31 @@
-__author__ = 'Ariel'
 __author__ = 'qPCR4vir'
+# DBox !!!!!!!!!!!!!!     tablet1234   PC !!
+
+tipMask=[]
+tipsMask=[]
+for tip in range(13):
+    tipMask += [1 << tip]
+    tipsMask += [2**tip - 1]
+
+
+
+class Robot:
+    class arm:
+            DiTi=0
+            Fixed=1
+
+        class tip:
+            def __init__(self, maxVol=1000)
+                    self.vol=0
+                    self.maxVol=maxVol
+                    
+            
+        def __init__(self, nTips, workingTips=None, tipsType=DiTi):
+            self.workingTips = workingTips if workingTips is not None else tipsMask[nTips]
+            self.tipsType = tipsType
+            self.nTips=nTips
+            self.
+
+        def drop(self, TIP_MASK=-1):
+            
+
