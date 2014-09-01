c73dcd8306a6f055a72627c21d4117b556efe06d
qPCR4vir <arielvina@yahoo.es>
2014-09-01 21:47:17 +0200

    fix: we want the 4 well

--- EvoScriPy/Example.py
+++ EvoScriPy/Example.py
@@ -10,6 +10,7 @@ from Instructions import *
 from Labware import *
 
 ElutionBuffer = Labware(Trough_100ml, Labware.Location(6, 0), "1-VEL-ElutionBuffer" )
+ElutionBuffer.select(range(2,2+4))
 LysisBuffer   = Labware(Trough_100ml, Labware.Location(6, 1), "2-Vl Lysis Buffer"   )
 BindingBuffer = Labware(Trough_100ml, Labware.Location(6, 2), "3-VEB Binding Buffer")
 VEW1          = Labware(Trough_100ml, Labware.Location(22,3), "4-VEW1 Wash Buffe"   )
