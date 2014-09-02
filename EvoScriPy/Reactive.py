50f85725fdd5a84a78a574f3fc68815d8f008df6
qPCR4vir <arielvina@yahoo.es>
2014-09-02 16:19:34 +0200

    working on Reactive and preMix

--- EvoScriPy/Reactive.py
+++ EvoScriPy/Reactive.py
@@ -1,1 +1,20 @@
 __author__ = 'qPCR4vir'
+
+import Labware
+
+class Reactive:
+    def __init__(self, name, labware, pos, volpersample):
+        self.name = name
+        self.labware = labware
+        self.pos = labware.offset(pos)
+        self.volpersample = volpersample
+        self.labware.Wells[self.pos].reactive=self
+
+
+class preMix(Reactive):
+    def __init__(self, name, labware, pos, components):
+        self.components = components
+        for react in components:
+
+
+
