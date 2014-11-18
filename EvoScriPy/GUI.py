eccd429ca358cbf9f38f9370a42b2b53ea3396b0
dondiablo <winterfeld.t87@googlemail.com>
2014-11-18 15:03:55 +0100

    working GUI

--- EvoScriPy/GUI.py
+++ EvoScriPy/GUI.py
@@ -2,23 +2,9 @@ __author__ = 'tobias.winterfeld'
 
 from tkinter import *
 
-master = Tk()
-'''frame = Frame(master)
-'''
-logo = PhotoImage(file="C:\Prog\EvoScriPy\FLI-Logo_mit_Farbverlauf.png")
-w1 = Label(master, image=logo).grid(row=0, column=1)
+root = Tk()
+logo = PhotoImage(file="../EvoScriPy/logo.gif")
+w = Label(root, image=logo)
+w.pack()
 
-class App:
-
-
-    def __init__(self, frame, buttons = ['START', 'STOPP', 'PAUSE', 'Get Variable', 'Get Volumina', 'Quit']):
-        frame2 = Frame(frame)
-        r = 0
-        frame2.grid(row=r, column=1)
-        for b in buttons:
-            self.button = Button(frame, text=buttons[r], command=frame.quit)
-            self.button.grid(row=r+1, column=0)
-            r += 1
-
-app = App(master)
-mainloop()
+root.mainloop()
