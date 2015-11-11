408c31ec6990724445327ce5d7dc8e11e6ab35aa
qPCR4vir <arielvina@yahoo.es>
2015-11-11 17:19:30 +0100

    add load, save and clear buttons for each of the 3 list, and use only 3 columns.

--- data/python/gui.py
+++ data/python/gui.py
@@ -20,45 +20,79 @@ class App(tkinter.Frame):
 
         tkinter.Button(self, text="Load original list of seq",
                              command=self.load_original)             .grid(row=0, column=0)
-        self.txt_original = scrolledtext.ScrolledText(self, height=h, width=w)
-        self.txt_original                                            .grid(row=1, column=0, sticky="nsew", padx=2, pady=2, columnspan=2)
+        self.txt_original = scrolledtext.ScrolledText(self,  width=w, height=h)#
+        self.txt_original                                            .grid(row=1, column=0, sticky=tkinter.NSEW, padx=2, pady=2)
+        tkinter.Button(self, text="clear",
+                             command=self.clear_original)            .grid(row=2, column=0)
+        tkinter.Button(self, text="Save",
+                             command=self.save_original)             .grid(row=3, column=0)
+
 
 
         tkinter.Button(self, text="Load ID to add",
-                             command=self.load_to_add)             .grid(row=0, column=2, columnspan=2)
-        self.txt_blast = scrolledtext.ScrolledText(self, height=h, width=w)
-        self.txt_blast                                               .grid(row=1, column=2, sticky="nsew", padx=2, pady=2, columnspan=2)
+                             command=self.load_to_add)               .grid(row=0, column=1)
+        self.txt_blast = scrolledtext.ScrolledText(self,  width=w)#, height=h+5
+        self.txt_blast                                               .grid(row=1, column=1, sticky=tkinter.NSEW, padx=2, pady=2)
         tkinter.Button(self, text="clear",
-                             command=self.clear_blast)               .grid(row=2, column=2)
+                             command=self.clear_blast)               .grid(row=2, column=1)
+        tkinter.Button(self, text="Save",
+                             command=self.save_blast)                .grid(row=3, column=1)
         tkinter.Button(self, text="BLAST",
-                             command=self.blast)                     .grid(row=2, column=3)
+                             command=self.blast)                     .grid(row=4, column=1)
 
 
-        tkinter.Button(self, text="Filter",
-                             command=self.filter)                    .grid(row=0, column=4, columnspan=2)
-        self.txt_unique = scrolledtext.ScrolledText(self, height=h, width=w)
-        self.txt_unique                                              .grid(row=1, column=4, sticky="nsew", padx=2, pady=2, columnspan=2)
+        tkinter.Button(self, text="Load",
+                             command=self.load_unique)               .grid(row=0, column=2)
+        self.txt_unique = scrolledtext.ScrolledText(self,  width=w)#, height=0
+        self.txt_unique                                              .grid(row=1, column=2, sticky=tkinter.NSEW, padx=2, pady=2)
         tkinter.Button(self, text="clear",
-                             command=self.clear_unique)              .grid(row=2, column=4)
+                             command=self.clear_unique)              .grid(row=2, column=2)
+        tkinter.Button(self, text="Save",
+                             command=self.save_unique)               .grid(row=3, column=2)
         tkinter.Button(self, text="Load BLAST",
-                             command=self.load_blast)                .grid(row=2, column=5)
+                             command=self.load_blast)                .grid(row=5, column=2)
+        tkinter.Button(self, text="Filter",
+                             command=self.filter)                    .grid(row=4, column=2)
+
+
+    def clear_txt(self, txt_list):
+        txt_list.delete(1.0, tkinter.END)
+
+    def clear_original(self):
+        self.clear_txt(self.txt_original)
 
     def clear_blast(self):
-        self.txt_blast.delete(1.0, tkinter.END)
+        self.clear_txt(self.txt_blast)
 
     def clear_unique(self):
-        self.txt_unique.delete(1.0, tkinter.END)
+        self.clear_txt(self.txt_unique)
+
+    def load_IDs(self, txt_list):
+        with filedialog.askopenfile(filetypes=(("TXT", "*.txt"), ("All files", "*.*") )) as ID_file:
+            for line in ID_file:
+                txt_list.insert(tkinter.END,line)
 
     def load_original(self):
-        with filedialog.askopenfile(filetypes=( ("TXT", "*.txt"), ("All files", "*.*") )) as original:
-            self.txt_original.delete(1.0, tkinter.END)
-            for line in original:
-                self.txt_original.insert(tkinter.END,line)
+        self.load_IDs(self.txt_original)
 
     def load_to_add(self):
-        with filedialog.askopenfile(filetypes=(("TXT", "*.txt"), ("All files", "*.*") )) as to_add_file:
-            for line in to_add_file:
-                self.txt_blast.insert(tkinter.END,line)
+        self.load_IDs(self.txt_blast)
+
+    def load_unique(self):
+        self.load_IDs(self.txt_unique)
+
+    def save_IDs(self, txt_list):
+        with filedialog.asksaveasfile(mode='w', filetypes=(("TXT", "*.txt"), ("All files", "*.*") )) as ID_file:
+            ID_file.write('\n'.join(txt_list.get('1.0',tkinter.END).splitlines()))
+
+    def save_original(self):
+        self.save_IDs(self.txt_original)
+
+    def save_blast(self):
+        self.save_IDs(self.txt_blast)
+
+    def save_unique(self):
+        self.save_IDs(self.txt_unique)
 
     def filter_add(self, add):
         ori  ={ID for ID in self.txt_original.get('1.0',tkinter.END).splitlines()}
