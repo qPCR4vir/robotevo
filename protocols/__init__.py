cb0fd32d858fcf828fe4ebeeb6511f4334d6fb6d
qPCR4vir <arielvina@yahoo.es>
2016-06-15 19:27:35 +0200

    rename dir to avoid package - module crash?

--- protocols/__init__.py
+++ protocols/__init__.py
@@ -4,6 +4,6 @@ Maintain here a list of all available costume protocols
 
 available = []
 
-import protocols.RNAextractionMN_Mag_Vet
+import protocols.RNAextractionMN_Mag
 from EvoScriPy.protocol_steps import Protocol
 available.append(Protocol)
