a571305619b2bdc1dd8739ad5140e6f732cad33b
qPCR4vir <arielvina@yahoo.es>
2016-06-15 13:05:14 +0200

    register available custom protocols

---
+++ protocols/__init__.py
@@ -0,0 +1,13 @@
+"""
+Maintain here a list of all available costume protocols
+"""
+
+available = []
+
+import protocols.RNAextractionMN_Mag_Vet
+
+print(available)
+from EvoScriPy.protocol_steps import Protocol
+
+available.append(Protocol)
+print(available)
