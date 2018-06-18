7731a702cf0c537cc4ba3589447e01c7866f02c9
qPCR4vir <arielvina@yahoo.es>
2018-06-18 13:54:00 +0200

    add PreKingFisher_RNAextNucleoMag to available protocols

--- protocols/__init__.py
+++ protocols/__init__.py
@@ -1,9 +1,17 @@
+# Copyright (C) 2014-2018, Ariel Vina Rodriguez ( Ariel.VinaRodriguez@fli.de , arielvina@yahoo.es )
+#  distributed under the GNU General Public License, see <http://www.gnu.org/licenses/>.
+#
+# protocols / __init__.py  : author Ariel Vina-Rodriguez (qPCR4vir)
+# 2014-2018
+
 """
-Maintain here a list of all available costume protocols
+Maintain here a list of all available customer protocols
 """
 
 available = []
 
 import protocols.RNAextractionMN_Mag
+import protocols.PreKingFisher_RNAextNucleoMag
+
 from EvoScriPy.protocol_steps import Protocol
 available.append(Protocol)
