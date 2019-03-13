8050edd92e4037dcedd828f3cb5a8b01ccbe4655
qPCR4vir <arielvina@yahoo.es>
2019-03-13 01:23:21 +0100

    add version samples in-VL inactivated and original samples

--- protocols/PreKingFisher_RNAextNucleoMag_EtOH80p.py
+++ protocols/PreKingFisher_RNAextNucleoMag_EtOH80p.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2018-2018, Ariel Vina Rodriguez ( Ariel.VinaRodriguez@fli.de , arielvina@yahoo.es )
+# Copyright (C) 2018-2019, Ariel Vina Rodriguez ( arielvina@yahoo.es )
 #  distributed under the GNU General Public License, see <http://www.gnu.org/licenses/>.
 #
 # author Ariel Vina-Rodriguez (qPCR4vir)
@@ -22,15 +22,15 @@ class PreKingFisher_RNAextNucleoMag_EtOH80p(Evo100_FLI):
     min_s, max_s = 1, 96
 
     def def_versions(self):
-        self.versions = {'in-plate inactivated': self.V_in_plate_inactivated,
+        self.versions = {'prefill inactivation': self.V_fill_inactivation,
+                         'in-VL inactivated'   : self.V_VL_inactivated,
                          'pre Inactivated'     : self.V_inactivated,
-                         'prefill inactivation': self.V_fill_inactivation,
-                         'prefill LysisBuffer' : self.V_fill_LysisBuffer                         }
+                         'original samples'    : self.V_original_samples                }
 
-    def V_in_plate_inactivated(self):
+    def V_VL_inactivated(self):
         pass
 
-    def V_fill_LysisBuffer(self):
+    def V_original_samples(self):
         pass
 
     def V_inactivated(self):
@@ -83,8 +83,13 @@ class PreKingFisher_RNAextNucleoMag_EtOH80p(Evo100_FLI):
         B_BeadsVolume       =  20.0
         EtOH80pVolume       = 600.0
         SampleVolume        = 100.0
-        if self.version == 'pre Inactivated':
-            SampleVolume  += ProtKVolume + cRNAVolume + IC_MS2Volume + LysisBufferVolume   # ????
+        InitLysisVol        = 0.0
+
+        if self.version == 'in-VL inactivated':
+            InitLysisVol  = SampleVolume + LysisBufferVolume
+
+        elif self.version == 'pre Inactivated':
+            InitLysisVol  = SampleVolume + ProtKVolume + cRNAVolume + IC_MS2Volume + LysisBufferVolume
 
 
         # Liquid classes used for pippetting. Others liquidClass names are defined in "protocol_steps.py"
@@ -98,7 +103,7 @@ class PreKingFisher_RNAextNucleoMag_EtOH80p(Evo100_FLI):
 
         # Define the reactives in each labware (Cuvette, eppys, etc.)
 
-        if self.version != 'pre Inactivated':
+        if self.version != 'pre Inactivated':             # we need to add ProtK+cRNA+MS2 mix
             ProtK       = Rtv.Reactive("Proteinase K ",
                                        Reactives,
                                        replicas     = 2,
@@ -118,7 +123,9 @@ class PreKingFisher_RNAextNucleoMag_EtOH80p(Evo100_FLI):
                                            defLiqClass=W_liquidClass,
                                            excess=20)
 
-            LysisBuffer     = Rtv.Reactive("VL - Lysis Buffer " , LysBuf,  volpersample=LysisBufferVolume , defLiqClass='MN VL')
+            if self.version != 'in-VL inactivated':
+                LysisBuffer     = Rtv.Reactive("VL - Lysis Buffer " , LysBuf,  volpersample=LysisBufferVolume , defLiqClass='MN VL')
+
 
 
         B_Beads         = Rtv.Reactive("B - Beads " ,
@@ -144,7 +151,7 @@ class PreKingFisher_RNAextNucleoMag_EtOH80p(Evo100_FLI):
         Plate_lysis = wt.getLabware(Lab.MP96deepwell,   "Plate lysis"   )  # Plate 12 x 8 ?
         Plate_EtOH  = wt.getLabware(Lab.MP96deepwell,   "Plate EtOH"    )  # Plate 12 x 8 ? MP96well !!
 
-        if self.version != 'prefill inactivation':
+        if self.version != 'original samples':
            Samples  = wt.getLabware(Lab.EppRack6x16,    "Proben"        )  # 6x16 = 12 x 8 ?
 
 
@@ -153,17 +160,15 @@ class PreKingFisher_RNAextNucleoMag_EtOH80p(Evo100_FLI):
 
         for s in all_samples:
 
-            if self.version != 'prefill inactivation':
+            if self.version == 'original samples':
                 Rtv.Reactive("probe_{:02d}".format(s + 1), Samples, single_use=SampleVolume,
                              pos=s + 1, defLiqClass=SampleLiqClass, excess=0)
 
-            Rtv.Reactive("lysis_{:02d}".format(s + 1), Plate_lysis, initial_vol=0.0, pos=s + 1,
-                         excess=0)  # todo revise order !!!
+            Rtv.Reactive("lysis_{:02d}".format(s + 1), Plate_lysis, initial_vol=InitLysisVol, pos=s + 1,
+                         excess=0)
 
             Rtv.Reactive("EtOH80p_{:02d}".format(s + 1), Plate_EtOH, initial_vol=0.0, pos=par[s] + 1,
                          excess=0)  # todo revise order !!!
-
-
 
 
         with group("Prefill plate with EtOH80p"):
@@ -172,15 +177,21 @@ class PreKingFisher_RNAextNucleoMag_EtOH80p(Evo100_FLI):
 
 
         with group("Sample Lysis"):
-            if self.version != 'pre Inactivated':
+            if self.version != 'pre Inactivated':                            #  add  ProtK+cRNA+MS2 mix
+
                 with self.tips(tipsMask=maxMask, reuse=True, drop=False, drop_last=True):
                     self.makePreMix(pK_cRNA_MS2)
                     self.spread  (  reactive=pK_cRNA_MS2,   to_labware_region= Plate_lysis.selectOnly(all_samples))
 
-                with self.tips(tipsMask=maxMask, reuse=True, drop=False, drop_last=True):
-                    self.spread  (  reactive=LysisBuffer,   to_labware_region= Plate_lysis.selectOnly(all_samples))
 
-            if self.version != 'prefill inactivation':
+                if self.version != 'in-VL inactivated':                      # add  LysisBuffer
+
+                    with self.tips(tipsMask=maxMask, reuse=True, drop=False, drop_last=True):
+                        self.spread  (  reactive=LysisBuffer,   to_labware_region= Plate_lysis.selectOnly(all_samples))
+
+
+            if self.version == 'original samples':                       # add samples
+
                 Itr.userPrompt("Please make sure the samples are in place").exec()
                 with self.tips(reuse=False, drop=True):
                     self.transfer(  from_labware_region= Samples,
