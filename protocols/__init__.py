c0b978bdac676cf07a2ce580e8c10031dcef0874
qPCR4vir <arielvina@yahoo.es>
2019-04-30 14:46:25 +0200

    PEP8 and docs

--- protocols/__init__.py
+++ protocols/__init__.py
@@ -1,21 +1,14 @@
-# Copyright (C) 2014-2018, Ariel Vina Rodriguez ( Ariel.VinaRodriguez@fli.de , arielvina@yahoo.es )
+# Copyright (C) 2014-2019, Ariel Vina Rodriguez ( arielvina@yahoo.es )
 #  distributed under the GNU General Public License, see <http://www.gnu.org/licenses/>.
 #
-# protocols / __init__.py  : author Ariel Vina-Rodriguez (qPCR4vir)
-# 2014-2018
+# author Ariel Vina-Rodriguez (qPCR4vir)
+# 2014-2019
+
+__author__ = 'qPCR4vir'
 
 """
 Maintain here a list of all available customer protocols
 """
-
-available = []
-available_classes = {}
-
-
-def registreExecutable(executable):
-    available.append(executable)
-    available_classes[executable.name] = executable.__class__
-
 
 from protocols.Prefill_plate_in_Evo200                import Prefill_plate_in_Evo200
 from protocols.RNAextractionMN_Mag_Vet                import RNAextr_MN_Vet_Kit
@@ -23,18 +16,28 @@ from protocols.Prefill_plates_VEW1_ElutionBuffer_VEW2 import Prefill_plates_VEW1
 from protocols.Prefill_plates_LysisBuffer             import Prefill_plates_LysisBuffer
 from protocols.PreKingFisher_RNAextNucleoMag_EtOH80p  import PreKingFisher_RNAextNucleoMag_EtOH80p
 from protocols.Prefill_plates_LysisBuffer_and_ProtKpreMix  import Prefill_plates_LysisBuffer_and_ProtKpreMix
-#from protocols.PreKingFisher_RNAextNucleoMag          import PreKingFisher_RNAextNucleoMag
 from EvoScriPy.protocol_steps                         import Pipeline
+from protocols.PCR                                    import PCRexperiment
+
+available         = []
+available_classes = {}
+
+available.append( Prefill_plate_in_Evo200                     ())
+available.append( Prefill_plates_VEW1_ElutionBuffer_VEW2      ())
+available.append( Prefill_plates_LysisBuffer                  ())
+available.append( Prefill_plates_LysisBuffer_and_ProtKpreMix  ())
+available.append( PreKingFisher_RNAextNucleoMag_EtOH80p       ())
+available.append( RNAextr_MN_Vet_Kit                          ())
+available.append( PCRexperiment                               ())
 
 
-available.append(Prefill_plate_in_Evo200                     ())
-available.append(Prefill_plates_VEW1_ElutionBuffer_VEW2      ())
-available.append(Prefill_plates_LysisBuffer                  ())
-available.append(Prefill_plates_LysisBuffer_and_ProtKpreMix  ())
-available.append(PreKingFisher_RNAextNucleoMag_EtOH80p       ())
-available.append(RNAextr_MN_Vet_Kit                          ())
-
-
+def registreExecutable(executable):
+    """
+    An alternative to direct registration in available
+    :param executable:
+    """
+    available.append(executable)
+    available_classes[executable.name] = executable.__class__
 
 
 pipeline_PreKingFisher = Pipeline ( protocols=  [Prefill_plates_VEW1_ElutionBuffer_VEW2 (run_name = "Prefill"),
@@ -42,10 +45,6 @@ pipeline_PreKingFisher = Pipeline ( protocols=  [Prefill_plates_VEW1_ElutionBuff
                                     run_name='PreKingFisher')
 
 registreExecutable(pipeline_PreKingFisher)
-
-from protocols.PCR import PCRexperiment
-available.append(PCRexperiment())
-
 available.append(Pipeline())
 
 
