0a44245f44be0bae31d6a570ed06db80be13d4c1
qPCR4vir <arielvina@yahoo.es>
2019-05-08 23:36:41 +0200

    first test ever: Prefill_VEW1_ElutB_and_VEW2_test_96s

--- protocols/Prefill_plates_VEW1_ElutionBuffer_VEW2.py
+++ protocols/Prefill_plates_VEW1_ElutionBuffer_VEW2.py
@@ -30,13 +30,19 @@ class Prefill_plates_VEW1_ElutionBuffer_VEW2(Evo100_FLI):
         pass
 
 
-    def __init__(self, GUI=None, run_name="Prefill VEW1 ElutB and VEW2"):
+    def __init__(self,
+                 GUI                            = None,
+                 NumOfSamples       : int       = None,
+                 worktable_template_filename    ='../EvoScripts/wt_templates/Prefill_VEW1_ElutB_and_VEW2.ewt',
+                 output_filename                ='../current/Prefill_plates_VEW1_ElutionBuffer_VEW2'  ,
+                 run_name: str                  = "Prefill VEW1 ElutB and VEW2"  ):
 
         Evo100_FLI.__init__(self,
                             GUI                     = GUI,
-                            NumOfSamples            = Prefill_plates_VEW1_ElutionBuffer_VEW2.max_s,
-                            worktable_template_filename='../EvoScripts/wt_templates/Prefill_VEW1_ElutB_and_VEW2.ewt',
-                            output_filename         ='../current/' + run_name,
+                            NumOfSamples            = NumOfSamples or Prefill_plates_VEW1_ElutionBuffer_VEW2.max_s,
+                            worktable_template_filename
+                                                    =worktable_template_filename,
+                            output_filename         = output_filename + run_name,
                             run_name                = run_name)
 
     def Run(self):
---
+++ protocols/tests.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2014-2019, Ariel Vina Rodriguez ( arielvina@yahoo.es )
+#  distributed under the GNU General Public License, see <http://www.gnu.org/licenses/>.
+#
+# author Ariel Vina-Rodriguez (qPCR4vir)
+# 2014-2019
+__author__ = 'qPCR4vir'
+
+from protocols.Prefill_plates_VEW1_ElutionBuffer_VEW2 import Prefill_plates_VEW1_ElutionBuffer_VEW2 as Prt
+p = Prt (NumOfSamples    = 96,
+         output_filename = '../current/tests/Prefill_plates_VEW1_ElutionBuffer_VEW2',
+         run_name        = "_test_96s")
+p.Run()
+
+
+
+
+#from protocols.Prefill_plate_in_Evo200                import Prefill_plate_in_Evo200
+#from protocols.RNAextractionMN_Mag_Vet                import RNAextr_MN_Vet_Kit
+#from protocols.Prefill_plates_LysisBuffer             import Prefill_plates_LysisBuffer
+#from protocols.PreKingFisher_RNAextNucleoMag_EtOH80p  import PreKingFisher_RNAextNucleoMag_EtOH80p
+#from protocols.Prefill_plates_LysisBuffer_and_ProtKpreMix  import Prefill_plates_LysisBuffer_and_ProtKpreMix
+#from EvoScriPy.protocol_steps                         import Pipeline
+#from protocols.PCR                                    import PCRexperiment
+
