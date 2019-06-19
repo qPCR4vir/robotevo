5e1386703b538b280f84ed6fb716b899153f0ffe
qPCR4vir <arielvina@yahoo.es>
2019-06-19 15:47:02 +0200

    tested in real robot - OK, with worktable added

--- protocols/hello_world.py
+++ protocols/hello_world.py
@@ -7,7 +7,10 @@ class HelloWorld(Protocol):
     name = "Hello World"
 
     def __init__(self, GUI = None):
-        Protocol.__init__(self, GUI= GUI, output_filename = '../current/tests/hello_world')
+        Protocol.__init__(self,
+                          GUI                           = GUI,
+                          output_filename               = '../current/tests/hello_world',
+                          worktable_template_filename   = '../EvoScripts/wt_templates/Prefill_VEW1_ElutB_and_VEW2.ewt')
 
     def Run(self):
         self.check_list()
